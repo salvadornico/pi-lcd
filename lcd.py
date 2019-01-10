208d893d1a80cff5400e3f8c2f971e186b5f04b0
Nico Salvador <salvador.nico@gmail.com>
2019-01-10 21:30:32 +0800

    Added lcd.py

---
+++ lcd.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import I2C_LCD_driver
+import fcntl
+import socket
+import struct
+import time
+
+mylcd = I2C_LCD_driver.lcd()
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(
+        fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                                    ifname[:15]))[20:24])
+
+
+while True:
+	mylcd.lcd_display_string("IP Address:", 1)
+	mylcd.lcd_display_string(get_ip_address('eth0'), 2)
+
+	time.sleep(1)
+	mylcd.lcd_clear()
+	time.sleep(1)
+
+	mylcd.lcd_display_string("Time: %s" % time.strftime("%H:%M:%S"), 1)
+	mylcd.lcd_display_string("Date: %s" % time.strftime("%m/%d/%Y"), 2)
+
+	time.sleep(1)
+	mylcd.lcd_clear()
+	time.sleep(1)
